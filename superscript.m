8faf18694dd6e222e12c9ada896bf3e59cdd3fe7
Stefan Sietzen <stefan.sietzen@gmx.at>
2013-10-21 15:31:45 +0200

    regionprops einlesen

--- superscript.m
+++ superscript.m
@@ -3,14 +3,16 @@ function [] = superscript(class)
 dirname = 'MPEG7_CE-Shape-1_Part_B';
 D = dir(dirname);
 I = cell(20, 1);
+STATS = cell(20, 1);
 j = 1;
 for i = 1 : size(D, 1)
     if D(i).isdir == 0 && strncmp(D(i).name(1:numel(class)), class, numel(class))
         disp(D(i).name);
         I_temp = imread(strcat(dirname, '/', [D(i).name]));
         I{j} = im2bw(I_temp, graythresh(I_temp));
-        imshow(I{j});
+        STATS{j} = regionprops(I{j}, 'all');
         j = j+1;
-        pause(1);
     end
 end
+
+STATS{3}
