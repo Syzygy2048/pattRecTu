281d9f8b1a880c158ee8b9158f9d53dd165b4227
Stefan Sietzen <stefan.sietzen@gmx.at>
2013-10-21 16:05:38 +0200

    Areas werden fr blobs 1 und 2 ausgegeben

--- superscript.m
+++ superscript.m
@@ -3,16 +3,20 @@ function [] = superscript(class)
 dirname = 'MPEG7_CE-Shape-1_Part_B';
 D = dir(dirname);
 I = cell(20, 1);
-STATS = cell(20, 1);
+L = cell(20, 1);
 j = 1;
 for i = 1 : size(D, 1)
     if D(i).isdir == 0 && strncmp(D(i).name(1:numel(class)), class, numel(class))
         disp(D(i).name);
         I_temp = imread(strcat(dirname, '/', [D(i).name]));
         I{j} = im2bw(I_temp, graythresh(I_temp));
-        STATS{j} = regionprops(I{j}, 'all');
+        L{j} = bwlabel(I{j}, 8);
+        STATS = regionprops(L{j}, 'all');
+        s = size(STATS);
+        STATS(1).Area
+        if s(1) > 1
+           STATS(2).Area
+        end
         j = j+1;
     end
 end
-
-STATS{3}
