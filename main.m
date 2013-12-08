625dfe2601f091e609f1658e8997c3b2c280ebf3
Syzygy <pollak.peter89@gmail.com>
2013-12-08 18:22:52 +0100

    cleaned project, input of new values working, parts of mahalanobis working
    moved old stuff that could possibly be reused into the "oldstuff" folder
    values from the wine data set are read and normalized
    covariance matrix for mahalanobis is calculated (possibly wrong, and
    possibly not allowed)

--- main.m
+++ main.m
@@ -1,3 +1,17 @@
-superscript;
-superscript2;
-superscript3;
+%{we need to read the data from the wine.data file and need to prepare it for evaluation:
+ the data is found in the following format:
+	1,14.23,1.71,2.43,15.6,127,2.8,3.06,.28,2.29,5.64,1.04,3.92,1065
+%} the first value is the the class, rest is values.
+
+%data is read into a matrix, each row is a data set, each column is a specific attribute, first column is class
+M = csvread('wine.data');
+
+%normalizing all values
+for i = 2:size(M,2)
+	M(:,i) = M(:,i)/norm(M(:,i));
+end
+
+
+mahalanobis(M);
+
+
