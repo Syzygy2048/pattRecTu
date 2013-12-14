a79d1a60ed9770010e33e417d99f4758d3543df9
Syzygy <pollak.peter89@gmail.com>
2013-12-14 18:55:02 +0100

    split data into learn and test sets, restarted mahalanobis, calculated mean of learn sets (for covariance)

--- mahalanobis.m
+++ mahalanobis.m
@@ -1,20 +1,27 @@
-%{this function calculates the mahalanobis distance
+%{
+	from: http://www.mathworks.de/de/help/stats/mahal.html
+	d = mahal(Y,X) computes the Mahalanobis distance (in squared units) of each observation in Y from the reference sample in matrix X.
+	If Y is n-by-m, where n is the number of observations and m is the dimension of the data, d is n-by-1. X and Y must have the same
+	number of columns, but can have different numbers of rows. X must have more rows than columns.
+	For observation I, the Mahalanobis distance is defined by d(I) = (Y(I,:)-mu)*inv(SIGMA)*(Y(I,:)-mu)', where mu and SIGMA are the
+	sample mean and covariance of the data in X. mahal performs an equivalent, but more efficient, computation.
 %}
-function [error] = mahalanobis(M)
 
-	correct = 0;
+function [error] = mahalanobis(trainA, trainB, trainC, test)
+	meanA = mean(trainA(:,2:end));
+	meanB = mean(trainB(:,2:end));
+	meanC = mean(trainC(:,2:end));
+	meanA
+	meanB
+	meanC
+	
 
-	covM = diag(cov(M(:,2:end))); %simplification possibly applied wrong | possibly not allowed, might need to implement this
+	
+	
+	
+	
+	
+	
 
-	for i = 1:size(M,1)
-		
-		for j = 1:size(M,1)
-			if j ~= i	%leave one out crossvalidation
-		
-			end
-		end
-
-	end
-
-	error = 1 - correct/size(M,1);
+	
 end
--- main.m
+++ main.m
@@ -1,7 +1,9 @@
-%{we need to read the data from the wine.data file and need to prepare it for evaluation:
- the data is found in the following format:
+%{
+	we need to read the data from the wine.data file and need to prepare it for evaluation:
+	the data is found in the following format:
 	1,14.23,1.71,2.43,15.6,127,2.8,3.06,.28,2.29,5.64,1.04,3.92,1065
-%} the first value is the the class, rest is values.
+	the first value is the the class, rest is values.
+%} 
 
 %data is read into a matrix, each row is a data set, each column is a specific attribute, first column is class
 M = csvread('wine.data');
@@ -11,7 +13,26 @@ for i = 2:size(M,2)
 	M(:,i) = M(:,i)/norm(M(:,i));
 end
 
+%splitting the values into a test and trainings set for mahalanobis
+% The dataset consists of 178 instances, such that the class 1 has 59 samples; class 2 has 71 samples; and class 3 has 48 samples. 
+trainA = [];
+trainB = [];
+trainC = [];
+test = [];
+for i = 1:size(M,1)
+	if(i<30)
+		trainA = [trainA; M(i,:)];
+    elseif(i < 60)
+		test = [test; M(i,:)];
+    elseif(i < 95)
+		trainB = [trainB; M(i,:)];
+    elseif(i < 131)
+		test = [test; M(i,:)];
+    elseif(i < 155)
+		trainC = [trainC; M(i,:)];
+	else
+		test = [test; M(i,:)];
+	end
+end
 
-mahalanobis(M);
-
-
+mahalanobis(trainA, trainB, trainC, test);
