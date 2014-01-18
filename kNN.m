587b21b90591036393417768c37121c67be787ba
Stefan Sietzen <stefan.sietzen@gmx.at>
2014-01-18 21:19:18 +0100

    test und trainingsset in main erstellen

--- kNN.m
+++ kNN.m
@@ -1,39 +1,31 @@
-function [] = kNN(data, classes, k)
+function [] = kNN(testData, trainingData, testClasses, trainingClasses, k)
 
-s = size(data);
-if mod(s(1),2) ~= 0
-   data = data(1:s(1)-1,:);
-   s = size(data);
-end
-
-% normalize data
-for i=1:s(2)
-    maximum = max(data(:,i));
-    data(:,i) = data(:,i)/maximum;
-end
 
 % create test set cells
 
-testSet = cell(s(1)/2,1);
+testSize = size(testData);
 
-for i=1:s(1)/2
-   c = cell(s(2)+1,1);
-   c{1} = int2str(classes(i*2));
-   for j=2:s(2)+1
-      c{j} = data(i, j-1); 
+testSet = cell(testSize(1),1);
+
+for i=1:testSize(1)
+   c = cell(testSize(2)+1,1);
+   c{1} = int2str(testClasses(i));
+   for j=2:testSize(2)+1
+      c{j} = testData(i, j-1); 
    end
    testSet{i} = c;
 end
 
 % create traing set
 
-trainingSet = cell(s(1)/2,1);
+trainingSize = size(trainingData);
+trainingSet = cell(trainingSize(1),1);
 
-for i=1:s(1)/2
-   c = cell(s(2)+1,1);
-   c{1} = int2str(classes(i*2-1));
-   for j=2:s(2)+1
-      c{j} = data(i, j-1); 
+for i=1:trainingSize(1)
+   c = cell(trainingSize(2)+1,1);
+   c{1} = int2str(trainingClasses(i));
+   for j=2:trainingSize(2)+1
+      c{j} = trainingData(i, j-1); 
    end
    trainingSet{i} = c;
 end
--- main.m
+++ main.m
@@ -21,7 +21,47 @@ perco(data, learndata2, 100000);
 %}
 
 load('data/strokefeatures.mat');
-kNNdata = features_class(:, 1:20);
-kNNclasses = features_class(:, 21);
-kNN(kNNdata, kNNclasses, 3);
+data = features_class(:, 1:20);
+
+% wir wollen gerade anzahl damit wir jeden 2. in test und trainingsset
+% stecken können
+s = size(data);
+data = data(1:s(1)-1,:);
+s = size(data);
+
+classes = features_class(:, 21);
+
+classes = classes(1:s(1)-1,:);
+
+% normalize data
+for i=1:s(2)
+    maximum = max(data(:,i));
+    data(:,i) = data(:,i)/maximum;
+end
+
+% create test set cells
+
+testSet = zeros(s(1)/2,s(2));
+testClasses = zeros(s(1)/2,1);
+
+for i=1:s(1)/2
+   for j=1:s(2)
+      testSet(i,j) = data(i*2, j);
+   end
+   testClasses(i) = data(i*2);   
+end
+
+% create traing set
+
+trainingSet = zeros(s(1)/2,s(2));
+trainingClasses = zeros(s(1)/2,1);
+
+for i=1:s(1)/2
+   for j=1:s(2)
+      trainingSet(i,j) = data(i*2-1, j); 
+   end
+   trainingClasses(i) = data(i*2-1);   
+end
+
+kNN(testSet, trainingSet, testClasses, trainingClasses, 3);
 
